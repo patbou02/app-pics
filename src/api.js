75d633ea7e1ed56c797fa23facf79172caa1a88c
Patrick Boutin <patrick.boutin@roberthalf.com>
2023-12-22 11:56:59 -0800

    api file tweaks
    small tweaks to the API js config file where the request is structured.

--- src/api.js
+++ src/api.js
@@ -1,8 +1,14 @@
 import axios from 'axios';
 
+const ACCESS_KEY = 'RoFoMpzx6JBLjGCMaopwaObdiKZ2LgJ4gAfIDhdlBq4';
+const LOCATION   = 'https://api.unsplash.com';
+const PATH       = '/search/photos';
+
+const URL_REQUEST = LOCATION + PATH;
+
 const searchImages = async (term) => {
-  const response = await axios.get('https://api.unsplash.com/search/photos', {
-    headers: { Authorization: 'Client-ID RoFoMpzx6JBLjGCMaopwaObdiKZ2LgJ4gAfIDhdlBq4' },
+  const response = await axios.get(URL_REQUEST, {
+    headers: { Authorization: `Client-ID ${ACCESS_KEY}` },
     params: { query: term }
   });
 
